11280ca5aa59e87fdb30d7056322b9f3dcc85c78
Nguon.Z <nzhong@appdev.yearup.org>
2023-04-28 12:21:28 -0500

    Product Class

---
+++ src/main/java/org/yearup/Product.java
@@ -0,0 +1,47 @@
+package org.yearup;
+
+public class Product
+{
+    private int Id;
+    private String Name;
+    private double Price;
+
+
+    public Product(int id, String name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public int getId()
+    {
+        return Id;
+    }
+
+    public void setId(int id)
+    {
+        Id = id;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public void setName(String name)
+    {
+        Name = name;
+    }
+
+    public double getPrice()
+    {
+        return Price;
+    }
+
+    public void setPrice(double price)
+    {
+        Price = price;
+    }
+
+}
